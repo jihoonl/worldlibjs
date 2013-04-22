4bfbf6a0ec95cef00f444473605acba8cbf2b35d
Jihoon Lee <jihoonlee.in@gmail.com>
2013-04-22 11:21:29 +0900

    compatible with roslib.js

--- worldlib.js
+++ worldlib.js
@@ -24,27 +24,27 @@ var Worldlib = function(options) {
   }
 
   // actionlib is used to communicate with the world_model
-  var cwoi = new ActionClient({
+  var cwoi = new ROSLIB.ActionClient({
     ros : worldlib.ros,
     serverName : '/spatial_world_model/create_world_object_instance',
     actionName : 'worldlib/CreateWorldObjectInstanceAction'
   });
-  var uwoi = new ActionClient({
+  var uwoi = new ROSLIB.ActionClient({
     ros : worldlib.ros,
     serverName : '/spatial_world_model/update_world_object_instance',
     actionName : 'worldlib/UpdateWorldObjectInstanceAction'
   });
-  var woits = new ActionClient({
+  var woits = new ROSLIB.ActionClient({
     ros : worldlib.ros,
     serverName : '/spatial_world_model/world_object_instance_tag_search',
     actionName : 'worldlib/WorldObjectInstanceTagSearchAction'
   });
-  var cwod = new ActionClient({
+  var cwod = new ROSLIB.ActionClient({
     ros : worldlib.ros,
     serverName : '/spatial_world_model/create_world_object_description',
     actionName : 'worldlib/CreateWorldObjectDescriptionAction'
   });
-  var gwod = new ActionClient({
+  var gwod = new ROSLIB.ActionClient({
     ros : worldlib.ros,
     serverName : '/spatial_world_model/get_world_object_description',
     actionName : 'worldlib/GetWorldObjectDescriptionAction'
@@ -53,8 +53,11 @@ var Worldlib = function(options) {
   // insert a new instance into the world model
   worldlib.createWorldObjectInstance = function(instance, callback) {
     // create the insertion goal
-    var goal = new cwoi.Goal({
-      instance : instance
+    var goal = new ROSLIB.Goal({
+      actionClient : cwoi,
+      goalMessage : {
+        instance : instance
+      } 
     });
     // define the callback
     goal.on('result', function(result) {
@@ -67,8 +70,11 @@ var Worldlib = function(options) {
   // search for all instances with the given array of tags
   worldlib.worldObjectInstanceTagSearch = function(tags, callback) {
     // create the search goal
-    var goal = new woits.Goal({
-      tags : tags
+    var goal = new ROSLIB.Goal({
+      actionClient : woits,
+      goalMessage : {
+        tags : tags
+      }
     });
     // define the callback
     goal.on('result', function(result) {
@@ -81,8 +87,11 @@ var Worldlib = function(options) {
   // get the WorldObjectDescription associated with the given ID
   worldlib.getWorldObjectDescription = function(descriptionId, callback) {
     // create the search goal
-    var goal = new gwod.Goal({
-      description_id : descriptionId
+    var goal = new ROSLIB.Goal({
+      actionClient : gwod,
+      goalMessage : {
+        description_id : descriptionId
+      }
     });
     // define the callback
     goal.on('result', function(result) {
@@ -95,8 +104,11 @@ var Worldlib = function(options) {
   //insert a new description into the world model
   worldlib.createWorldObjectDescription = function(description, callback) {
     // create the insertion goal
-    var goal = new cwod.Goal({
-      description : description
+    var goal = new ROSLIB.Goal({
+      actionClient : cwod, 
+      goalMessage : {
+        description : description
+      }
     });
     // define the callback
     goal.on('result', function(result) {
